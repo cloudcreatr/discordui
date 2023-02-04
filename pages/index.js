b882d40d2f2e74d4604717cbb554b94048b7cbc8
om <cloudcreatr@gmail.com>
2023-02-04 18:25:39 +0000

    nothnigr

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter, Libre_Barcode_128 } from "@next/font/google";
-
 export default function Home() {
   return (
-    <div className="flex h-[44rem] w-screen flex-col items-center justify-start bg-primary">
+    <div className="relative flex h-[90vh] w-screen flex-col items-center justify-start bg-primary">
       <nav className=" box-border flex h-24 w-11/12 max-w-[78.9rem]  flex-row items-center justify-between">
         <img className="w-35" src="./assets/discord_main_logo.svg"></img>
         <ul className="flex flex-row space-x-8 text-white">
@@ -21,21 +17,33 @@ export default function Home() {
         </button>
       </nav>
 
-      <section className="grid h-3/5 w-7/12 max-w-[78.9rem] grid-cols-4 grid-rows-3 items-center justify-items-center bg-slate-300">
-        <h1 className="col-span-4 text-2xl text-black ">IMAGINE A PLACE</h1>
-        <p className="col-span-4 w-4/5 text-center text-2xl text-black">
+      <section className=" grid h-3/5 w-11/12 max-w-[78.9rem] grid-cols-4 grid-rows-3 items-center justify-items-center">
+        <h1 className="col-span-4 text-7xl font-extrabold text-white ">
+          IMAGINE A PLACE
+        </h1>
+        <p className="col-span-4 w-4/5 text-center text-2xl font-semibold text-white">
           ...where you can belong to a school club, a gaming group, or a
           worldwide art community. Where just you and a handful of friends can
           spend time together. A place that makes it easy to talk every day and
           hang out more often.
         </p>
-        <button className=" col-span-2 m-5 box-border w-full rounded-full bg-white py-5 px-12 text-xl">
+        <button className=" col-span-2 mr-2  w-4/5 justify-self-end rounded-full bg-white py-5 px-12 text-xl">
           Download for Windows
         </button>
-        <button className="col-span-2 w-full rounded-full bg-black py-5 px-9 text-xl text-white">
+        <button className="col-span-2 ml-2 w-4/5 justify-self-start rounded-full bg-black py-5 px-9 text-xl text-white">
           Download for Windows
         </button>
       </section>
+
+      <div className="absolute bottom-0 w-[3000px]">
+        <img src="./assets/center_bg_landing_header.svg"></img>
+      </div>
+      <div className="buttom-0 w-11/12 self-end">
+        <img src="./assets/right_bg_landing_header.svg"></img>
+      </div>
+      <div>
+        <img className="" src="./assets/left_bg_landing_header.svg"></img>
+      </div>
     </div>
   );
 }
---
+++ pages/test.js
@@ -0,0 +1,10 @@
+export default function test() {
+  return (
+    <div className=" w-[100rem] bg-primary">
+      <img
+        className="w-[1000px] object-cover"
+        src="./assets/center_bg_landing_header.svg"
+      ></img>
+    </div>
+  );
+}
